f0fdd570f2543ee95bb7571422a1710d5ea7ba26
elbarto <amoghkatti9496@gmail.com>
2023-05-11 16:27:10 -0500

    Pushing new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Lets get started
+          Let's get started!
         </p>
       </header>
     </div>
