71f97232d2e5e3023aef19922c3d70dd289e5cd1
elbarto <amoghkatti9496@gmail.com>
2023-05-12 16:05:56 -0500

    Pushing composition code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
-
+import Card from './UI/Card'
 /**
  * You can see that this would throw an error -> You cannot have two root elements in one JSX file
  * Why is not allowed? Later we'll know 
  */
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{props.title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+            </Card>
     )
 }
 
