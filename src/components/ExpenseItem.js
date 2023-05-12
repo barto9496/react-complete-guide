a996186bed5e74afcdda4c7bf0bf89da0bfb1670
elbarto <amoghkatti9496@gmail.com>
2023-05-12 00:22:22 -0500

    Pushing overall section 3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 /**
  * You can see that this would throw an error -> You cannot have two root elements in one JSX file
  * Why is not allowed? Later we'll know 
  */
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 4, 28).toLocaleDateString();
-    const expenseTitle = 'Car insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div>
-                {expenseDate}
-            </div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     )
